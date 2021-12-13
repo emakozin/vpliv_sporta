a5ebe121faee4ef2f991a255c13f66cb12c01734
emakozin <ema.kozin@student.fmf.uni-lj.si>
2021-12-13 18:51:46 +0100

    test

---
+++ lib/libraries.r
@@ -0,0 +1,16 @@
+library(knitr)
+library(rvest)
+library(gsubfn)
+library(tidyr)
+library(tmap)
+library(shiny)
+library(readr)
+library(dplyr)
+library(tibble)
+
+
+
+options(gsubfn.engine="R")
+
+# Uvozimo funkcije za pobiranje in uvoz zemljevida.
+source("lib/uvozi.zemljevid.r", encoding="UTF-8")
