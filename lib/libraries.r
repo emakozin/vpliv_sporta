00ea333a661e072bdb7cd0002a00e4a8b2ab2c43
emakozin <ema.kozin@student.fmf.uni-lj.si>
2022-09-07 15:49:11 +0200

    popravki

--- lib/libraries.r
+++ lib/libraries.r
@@ -7,7 +7,17 @@ library(shiny)
 library(readr)
 library(dplyr)
 library(tibble)
-
+library(readxl)
+library(tidyr)
+library(ggplot2)
+library(tidyverse)
+library(lemon)
+library(ggvis)
+library(rgdal)
+library(mosaic)
+library(ggmap)
+library(mapproj)
+library(munsell)
 
 
 options(gsubfn.engine="R")
