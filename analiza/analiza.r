7af6d35f1e76138d2ab65485700761523a567a12
emakozin <ema.kozin@student.fmf.uni-lj.si>
2022-09-12 17:54:35 +0200

    popravki

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -5,11 +5,19 @@ library(reshape2)
 
   
 odstopanja <- data.frame(Odstop.pric.leta =  round(skupaj$PRIČAKOVANA_LETA - as.numeric(mean(skupaj$PRIČAKOVANA_LETA)),2), 
-                           Odstop.kolo = round(skupaj$KOLO - as.numeric(mean(skupaj$KOLO)),2), 
-                           Odstop.aero.sporti = round(skupaj$AEROBIČNI_ŠPORTI - as.numeric(mean(skupaj$AEROBIČNI_ŠPORTI)),2),
-                           Država = skupaj$Država) 
+                         Odstop.kolo = round(skupaj$KOLO - as.numeric(mean(skupaj$KOLO)),2), 
+                         Odstop.aero.sporti = round(skupaj$AEROBIČNI_ŠPORTI - as.numeric(mean(skupaj$AEROBIČNI_ŠPORTI)),2), 
+                         Odstop.dobro = round(skupaj$OCENA_DOBRO- as.numeric(mean(skupaj$OCENA_DOBRO)),2), 
+                         Odstop.slabo = round(skupaj$OCENA_SLABO - as.numeric(mean(skupaj$OCENA_SLABO)),2), 
+                         Država = skupaj$Država)
 
-mdfr <- melt(odstopanja, id.vars = "Država") %>% filter(Država != "Finland")
+mdfr <- melt(odstopanja, id.vars = "Država") %>% filter(!Država %in% c("Finland", "Norway"))
 
-graf9 <- ggplot(mdfr, aes(Država, value, fill = variable)) +  geom_bar(position = "dodge", stat = "identity") + labs(title = "Odstopanja od povprečja razlik v pričakovanih letih, deležu kolesarjev in prebivalstva v aerobičnih športih") + ylab("Odstop od povprečja") + scale_fill_discrete(labels=c("Odstop pričakovanih let", "Odstop kolesarjenja", "Odstop pri aerobičnih športih")) + theme(legend.title = element_blank(),axis.text = element_text(angle = 90))
+
+graf9 <- ggplot(mdfr, aes(Država, value, fill = variable)) +  
+  geom_bar(position = "dodge", stat = "identity") + 
+  labs(title = "Odstopanja od povprečja razlik v pričakovanih letih, deležu kolesarjev in prebivalstva v aerobičnih športih") + 
+  ylab("Odstop od povprečja") + 
+  scale_fill_discrete(labels=c("Odstop pričakovanih let", "Odstop kolesarjenja", "Odstop pri aerobičnih športih", "Odstop ocene dobro", "Odstop ocene slabo")) + 
+  theme(legend.title = element_blank(),axis.text = element_text(angle = 90))
   
